7994b40bd91b9469de61c7201789639c83cf268e
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-06-30 10:08:15 -0300

    close menu button functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let openMenu = document.getElementById('open-menu')
-let closeMenu = document.getElementById('close-menu')
+let closeMenu = document.getElementById('closeMenu')
 
 openMenu.addEventListener('click', () => {
     document.body.classList.add('menu-expanded')
